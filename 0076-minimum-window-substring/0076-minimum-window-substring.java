a6d440659d4214b4e6558960974c5f5109d15ae2
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-04-08 19:27:08 +0530

    Time: 20 ms (34.18%), Space: 45.3 MB (33.86%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,51 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        if (t.equals("")) {
+            return "";
+        }
+
+        Map<Character, Integer> countT = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+
+        for (char c : t.toCharArray()) {
+            countT.put(c, countT.getOrDefault(c, 0) + 1);
+        }
+
+        int have = 0, need = countT.size();
+        int[] res = new int[]{-1, -1};
+        int resLen = Integer.MAX_VALUE;
+        int l = 0;
+
+        for (int r = 0; r < s.length(); r++) {
+            char c = s.charAt(r);
+            window.put(c, window.getOrDefault(c, 0) + 1);
+
+            if (countT.containsKey(c) && window.get(c).equals(countT.get(c))) {
+                have++;
+            }
+
+            while (have == need) {
+                // Update the result
+                if (r - l + 1 < resLen) {
+                    res[0] = l;
+                    res[1] = r;
+                    resLen = r - l + 1;
+                }
+                // Pop from the left of the window
+                char leftChar = s.charAt(l);
+                window.put(leftChar, window.get(leftChar) - 1);
+                if (countT.containsKey(leftChar) && window.get(leftChar) < countT.get(leftChar)) {
+                    have--;
+                }
+                l++;
+            }
+        }
+
+        int left = res[0];
+        int right = res[1];
+        return resLen != Integer.MAX_VALUE ? s.substring(left, right + 1) : "";
+    }
+}
