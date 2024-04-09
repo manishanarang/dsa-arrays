425b79126051935a96409bac8b4327a9b6c70012
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-04-10 01:21:54 +0530

    Time: 0 ms (100.00%), Space: 41 MB (81.22%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int left =0, right=s.length()-1;
+        int open=0,close=0;
+        
+        while(left<s.length() || right>=0){
+            if(s.charAt(left) == '(' || s.charAt(left) == '*'){
+                open++;
+            }
+            else{
+                open--;
+            }
+            
+            if(s.charAt(right) == ')' || s.charAt(right) == '*'){
+                close++;
+            }
+            else{
+                close--;
+            }
+            
+            left++;
+            right--;
+            
+            if(open < 0 || close <0) return false;
+        }
+            return true;
+        
+    }
+}
