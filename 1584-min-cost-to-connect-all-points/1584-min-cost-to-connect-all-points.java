2ee1d39a35b1e51942b6609cd77adfeec1983c14
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-04-10 20:27:58 +0530

    Time: 632 ms (17.71%), Space: 59.5 MB (65.53%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.java
@@ -0,0 +1,63 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        int cost = 0;
+        
+        PriorityQueue<int[]> edges = new PriorityQueue<>((a,b)->a[2]-b[2]);
+        for(int i = 0; i<n;i++){
+            for(int j=i+1;j<n;j++){
+               int wt = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
+                edges.add(new int[]{i,j,wt});
+            }
+        }
+        
+        int processed = 0;
+        UnionFind uf = new UnionFind(n);
+        while(!edges.isEmpty() && processed<n){
+            int[] edge = edges.poll();
+            if(uf.union(edge[0], edge[1])){
+                cost+=edge[2];
+                processed++;
+            }
+        }
+        return cost;
+    }
+
+    class UnionFind{
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(int n){
+            parent = new int[n+1];
+            rank = new int[n+1];
+            for(int i = 0; i <=n; i++){
+                this.parent[i] = i;
+                this.rank[i] = 1;
+            }
+        }
+
+        public int find(int x){
+            if(x!=parent[x]){
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        public boolean union(int x, int y){
+            int p1 = find(x);
+            int p2 = find(y);
+
+            if(p1 == p2) return false;
+
+            if(rank[p1] > rank[p2]){
+                parent[p2] = p1;
+                rank[p1] = rank[p2] ;
+            }
+            else{
+                parent[p1] = p2;
+                rank[p2] = rank[p1] ;
+            }
+            return true;
+        }
+    }
+}
