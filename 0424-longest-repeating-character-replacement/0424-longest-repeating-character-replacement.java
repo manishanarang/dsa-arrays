86d7d00b8c16f39f7adafbf597924456dde6d271
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-05-06 22:52:45 +0530

    Time: 27 ms (28.17%), Space: 44.5 MB (18.64%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -4,15 +4,12 @@ class Solution {
         
         int left = 0; 
         int result = 0;
-        
+        int maxFreq=-1;
         for(int i = 0; i<s.length() ;i++){
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i),0)+1);
             
-            int maxFreq = map.values()
-                .stream()
-                .mapToInt(l->l)
-                .max().getAsInt();
-            while((i-left+1) - maxFreq > k){
+             maxFreq = Math.max(maxFreq, map.get(s.charAt(i)));
+            while((i-left+1) - maxFreq > k){ //invalid window
                 map.put(s.charAt(left), map.get(s.charAt(left))-1);
                 left++;
             }
