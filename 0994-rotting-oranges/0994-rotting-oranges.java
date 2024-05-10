6414a128abc01f23d033dd1801005cb6e14188d8
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-05-10 22:20:52 +0530

    Time: 2 ms (72.59%), Space: 42.3 MB (46.29%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int orangesRotting(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int dir_x[] = new int[]{1, -1, 0,0};
+        int dir_y[] = new int[]{0, 0, 1,-1};
+        int time =0, freshOranges = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[m][n];
+
+        for(int i = 0; i<m; i++){
+            for(int j = 0; j <n; j++){
+                if(grid[i][j] == 1){
+                    freshOranges++;
+                }
+                if(grid[i][j] == 2){
+                    queue.add(new int[]{i,j});
+                    visited[i][j]=true;
+                }
+            }
+        }
+        
+
+        while(!queue.isEmpty() && freshOranges > 0){
+            int sz = queue.size();
+            for(int l = 0; l < sz; l++){
+                        int[] rotten = queue.remove();
+                        int row = rotten[0];
+                        int col = rotten[1];
+
+                        for(int k = 0; k < 4; k++){
+                            int new_row = row + dir_x[k];
+                            int new_col = col + dir_y[k];
+                        if(isValid(new_row, new_col, m,n) && grid[new_row][new_col]==1 && !visited[new_row][new_col]){
+                            freshOranges--;
+                            visited[new_row][new_col]=true;
+                            grid[new_row][new_col] = 2;
+                            queue.add(new int[]{new_row,new_col});
+                        }
+                        }
+                    }
+                    time++;
+        }
+    
+
+    
+            if(freshOranges == 0){
+                return time;
+            }
+        
+        return -1;
+    }
+
+    public boolean isValid(int i, int j, int m, int n){
+        return (i<m && j <n && i >= 0 && j >= 0);
+    }
+}
