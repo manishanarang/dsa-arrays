da0c0de6ddbc0b4ac97edeb19a9b2b5abe713399
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-05-11 01:28:48 +0530

    Time: 6 ms (48.29%), Space: 45.6 MB (28.94%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,42 @@
+class Solution {
+  public boolean canFinish(int numCourses, int[][] prerequisites) {
+
+    HashMap<Integer, List<Integer>> courseDict = new HashMap<>();
+
+      for(int i=0;i<numCourses;i++)
+            courseDict.put(i, new ArrayList<>());
+    for (int[] relation : prerequisites) {
+      courseDict.computeIfAbsent(relation[1], k -> new ArrayList<>()).add(relation[0]);
+    }
+
+    Set<Integer> visited = new HashSet<>();
+
+    for (int i = 0; i < numCourses; ++i) {
+      if (isCyclic(i, courseDict, visited, new HashSet<>()))
+        return false;
+    }
+
+    return true;
+  }
+
+
+  protected boolean isCyclic(
+      Integer i, HashMap<Integer, List<Integer>> courseDict,
+      Set<Integer> visited, Set<Integer> cycle) {
+
+    if (cycle.contains(i))
+      return true;
+      
+    if(visited.contains(i)) return false;
+      
+    visited.add(i);
+    cycle.add(i);
+
+    for (Integer child : courseDict.get(i)) {
+            if(isCyclic(child, courseDict, visited, cycle)) return true;
+        }
+    
+    cycle.remove(i);
+    return false;
+}
+}
