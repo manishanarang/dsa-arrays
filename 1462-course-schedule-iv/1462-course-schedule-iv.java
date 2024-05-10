ae9294208df953badbc775c4cb98ebc52b38589b
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-05-11 01:53:42 +0530

    Time: 797 ms (5.05%), Space: 50 MB (12.00%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.java
+++ 1462-course-schedule-iv/1462-course-schedule-iv.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Solution {
     private Map<Integer, Set<Integer>> memo; // Memoization map to store results
     
-    private boolean dfs(int i, int j, List<List<Integer>> edge, int[] vis) {
+    private boolean dfs(int i, int j, Map<Integer, List<Integer>> graph, int[] vis) {
         if (i == j)
             return true;
         
         vis[i] = 1;
-        for (int it : edge.get(i)) {
+        for (int it : graph.get(i)) {
             if (vis[it] == 0) {
-                if (dfs(it, j, edge, vis)) {
+                if (dfs(it, j, graph, vis)) {
                     memo.computeIfAbsent(i, k -> new HashSet<>()).add(j); // Store the result in memoization map
                     return true;
                 }
@@ -23,20 +23,20 @@ public class Solution {
     }
 
     public List<Boolean> checkIfPrerequisite(int numCourses, int[][] prerequisites, int[][] queries) {
-        List<List<Integer>> edge = new ArrayList<>(numCourses);
+        Map<Integer, List<Integer>> graph = new HashMap<>();
         for (int i = 0; i < numCourses; i++) {
-            edge.add(new ArrayList<>());
+            graph.put(i, new ArrayList<>());
         }
         List<Boolean> ans = new ArrayList<>();
         memo = new HashMap<>();
         
         for (int[] pre : prerequisites) {
-            edge.get(pre[0]).add(pre[1]);
+            graph.computeIfAbsent(pre[0], k-> new ArrayList<>()).add(pre[1]);
         }
 
         for (int query[] : queries) {
             int[] vis = new int[numCourses];
-            boolean t = dfs(query[0], query[1], edge, vis);
+            boolean t = dfs(query[0], query[1], graph, vis);
             ans.add(t);
         }
         return ans;
