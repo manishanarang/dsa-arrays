0142258dac5941d9680d807eb8856ef3983cc35d
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-08-29 13:26:09 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (46.87%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -3,7 +3,7 @@ class Solution {
         int left =0, right=s.length()-1;
         int open=0,close=0;
         
-        while(left<s.length() || right>=0){
+        while(left<s.length() && right>=0){
             if(s.charAt(left) == '(' || s.charAt(left) == '*'){
                 open++;
             }
