05f58d8e7af90813e75d5dc1a0b7f88e706b9be4
manishanarang <160212455+manishanarang@users.noreply.github.com>
2024-08-29 17:37:52 +0530

    Time: 15 ms (43.37%), Space: 48.9 MB (48.06%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -22,13 +22,16 @@ class Solution {
             for (int j = 1; j <=n; j++) {
                 
                 // Remember, we always need this result
-                dp[i][j] = dp[i - 1][j];
+                
 
                 // If the characters match, we add the
                 // result of the next recursion call (in this
                 // case, the value of a cell in the dp table
                 if (s.charAt(i-1) == t.charAt(j-1)) {
-                    dp[i][j] += dp[i - 1][j - 1];
+                    dp[i][j]  =  dp[i - 1][j] + dp[i - 1][j - 1];
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
